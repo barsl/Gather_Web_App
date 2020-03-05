6dfddd5b157884a85351e9ac46e3c0571c81b986
Bar Slutsky <bslutsky@gmail.com>
2020-03-05 16:41:08 -0500

    Update server.js

--- gather_alpha/backend/server.js
+++ gather_alpha/backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://barsl:Mon141592654!%40%23@cluster0-ilww9.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://group:12345!%40%23@cluster0-ilww9.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
