d707d7c466ebb38566f24e41bdfd228e070b97c6
chenja17 <jasonutsc.chen@mail.utoronto.ca>
2020-03-07 11:22:08 -0500

    added fields to user model user model and began implementing http requests

--- gather_alpha/backend/server.js
+++ gather_alpha/backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.DEV_URI;
+const uri = "mongodb+srv://group:12345@cluster0-8mim2.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
