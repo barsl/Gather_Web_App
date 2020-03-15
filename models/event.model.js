ea341447a611ac4698de9b8f29615968c87de41d
chenja17 <jasonutsc.chen@mail.utoronto.ca>
2020-03-15 15:08:20 -0400

    implemented adding markers functionality to the map

--- models/event.model.js
+++ models/event.model.js
@@ -3,16 +3,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+<<<<<<< HEAD
   public: {type: Boolean, required: true},
   title: {type: String, required: true},
+=======
+  // public: {type: Boolean, required: true}, TODO: implement?
+  public: { type: Boolean, required: false },
+  title: { type: String, required: true },
+>>>>>>> implemented adding markers functionality to the map
   username: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
   // location: {type: String, required: true}, TODO: Uncomment when implemented
+<<<<<<< HEAD
   location: {type: String, required: false},
   invited: [{type: Schema.Types.ObjectId, ref: 'User', required: true }],
   attending: [{type: Schema.Types.ObjectId, ref: 'User', required: false}],
   tags: {type: [String], required: false}
+=======
+  location: { type: [[Number]], required: false },
+  invited: { type: [String], required: true },
+  attending: { type: [String], required: false },
+  tags: { type: [String], required: false }
+>>>>>>> implemented adding markers functionality to the map
   // TODO: add media
 }, {
   timestamps: true,
