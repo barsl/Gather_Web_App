b08b8d609553453c0dfc8366ddcdcda0210f3d73
Dasha Besshaposhnikova <dasha.besshaposhnikova@gmail.com>
2020-03-29 19:11:18 -0400

    Backend cleanup

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 function auth(req, res, next) {
-    if (!req.session.username) return res.status(401).json("Access Denied. Please login to continue");
+    if (!req.session.user) return res.status(401).json("Access Denied. Please login to continue");
     next();
 }
 
