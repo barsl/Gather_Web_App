7885025ff3448516dc1008d80c5aba3d5d498b28
Dasha Besshaposhnikova <dasha.besshaposhnikova@gmail.com>
2020-03-29 19:11:40 -0400

    Update app.js with new routes and AuthProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/login.component";
 import Signup from "./components/signup.component";
-import EventsList from "./components/events-list.component";
-import EditEvent from "./components/edit-event.component";
-import CreateEvent from "./components/create-event.component";
+import EventsList from "./components/events/events-list.component";
+import EditEvent from "./components/events/edit-event.component";
+import CreateEvent from "./components/events/create-event.component";
 import CreateUser from "./components/create-user.component";
 import Friends from "./components/friends.component";
+import UserProfile from "./components/user-profile.component";
+import OAuthCallback from "./components/auth/oauth/OAuthCallback";
+import { AuthProvider } from "./components/auth/context/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <br />
-        <Route exact path="/" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/eventsList" component={EventsList} />
-        <Route path="/edit/:id" component={EditEvent} />
-        <Route path="/create" component={CreateEvent} />
-        <Route path="/user" component={CreateUser} />
-        <Route path="/friends" component={Friends} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="container">
+          <br />
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/eventsList" component={EventsList} />
+          <Route path="/edit/:id" component={EditEvent} />
+          <Route path="/create" component={CreateEvent} />
+          <Route path="/user" component={CreateUser} />
+          <Route path="/friends" component={Friends} />
+          <Route path="/profile" component={UserProfile} />
+          <Route path="/oauthcallback" component={OAuthCallback} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
