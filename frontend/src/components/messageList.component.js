0b53439ca612b2c9f16a21422f9223cd90456e80
Dasha Besshaposhnikova <dasha.besshaposhnikova@gmail.com>
2020-04-12 18:30:29 -0400

    Improve chat interface

--- frontend/src/components/messageList.component.js
+++ frontend/src/components/messageList.component.js
@@ -1,30 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, createRef} from 'react';
 import classes from './style/chat.module.css';
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+    this.messagesView = createRef();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.messages !== this.props.messages) {
+      this.messagesView.current.scrollTo(
+        0,
+        this.messagesView.current.scrollHeight,
+      );
+    }
+  }
+
   render() {
     return (
-      <div className={classes.MessagesView}>
+      <div ref={this.messagesView} className={classes.MessagesView}>
         {this.props.messages.map((message, index) => {
           if (message.senderId === this.props.userName) {
             return (
               <div className={classes.Message} key={index}>
-                
-                  <span className={classes.SenderName}>{message.senderId}</span>
-                  <div className={classes.MessageBox}>{message.text}</div>
-                
+                <span className={classes.SenderName}>{message.senderId}</span>
+                <div className={classes.MessageBox}>{message.text}</div>
               </div>
             );
-          }
-          else {
+          } else {
             return (
-                <div className={classes.ReceivedMessage} key={index}>
-                  
-                    <span className={classes.ReceivedSenderName}>{message.senderId}</span>
-                    <div className={classes.ReceivedMessageBox}>{message.text}</div>
-                  
-                </div>
-              );
+              <div className={classes.ReceivedMessage} key={index}>
+                <span className={classes.ReceivedSenderName}>
+                  {message.senderId}
+                </span>
+                <div className={classes.ReceivedMessageBox}>{message.text}</div>
+              </div>
+            );
           }
         })}
       </div>
